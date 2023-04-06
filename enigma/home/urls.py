7afaa981af660051051db8b8c29fe9c3fcb58114
boris1592 <buzin.boria@gmail.com>
2023-04-06 22:04:02 +0300

    Formatted every file

--- enigma/home/urls.py
+++ enigma/home/urls.py
@@ -4,9 +4,5 @@ from django.urls import path
 app_name = 'home'
 
 urlpatterns = [
-    path(
-        '',
-        TemplateView.as_view(template_name='index.html'),
-        name='home'
-    )
+    path('', TemplateView.as_view(template_name='index.html'), name='home')
 ]
