a468d4a72befe0f54ed251a7c1e8fbff034346a5
boris1592 <buzin.boria@gmail.com>
2023-04-06 23:57:34 +0300

    Fixed reverse rotor pass

--- emulator/emulator.py
+++ emulator/emulator.py
@@ -1,39 +1,32 @@
 from string import ascii_uppercase
 
 
+def pairs_to_dict(pairs):
+    answer = {}
+
+    for pair in pairs:
+        answer[pair[0]] = pair[1]
+        answer[pair[1]] = pair[0]
+
+    return answer
+
+
 class EnigmaEmulator:
     def __init__(
         self,
         rotors: list[str],
-        reflector: str,
-        plugs: list[tuple[str, str]],
+        reflector: list[str],
+        plugs: list[str],
         positions: list[int],
         alphabet=ascii_uppercase,
     ):
         self.__alphabet = alphabet
         self.__positions = positions
-        self.__reflector = {
-            letter: reflector[i] for i, letter in enumerate(alphabet)
-        }
-        self.__rotors = list(
-            map(
-                lambda rotor: {
-                    letter: rotor[i] for i, letter in enumerate(alphabet)
-                },
-                rotors,
-            )
+        self.__rotors = [pairs_to_dict(rotor) for rotor in rotors]
+        self.__reflector = pairs_to_dict(reflector)
+        self.__plugs = pairs_to_dict(
+            [(letter, letter) for letter in alphabet] + plugs
         )
-        self.__plugs = {}
-
-        for letter in alphabet:
-            plug = next((p for p in plugs if letter in p), None)
-            self.__plugs[letter] = (
-                letter
-                if plug is None
-                else plug[0]
-                if letter == plug[1]
-                else plug[1]
-            )
 
     def __rotate_rotors(self):
         self.__positions[0] += 1
@@ -47,14 +40,20 @@ class EnigmaEmulator:
             if i < len(self.__positions) - 1:
                 self.__positions[i + 1] += 1
 
+    def __pass_through_rotor(self, rotor_index, letter):
+        letter_index = (
+            self.__alphabet.find(letter) + self.__positions[rotor_index]
+        ) % len(self.__alphabet)
+        return self.__rotors[rotor_index][self.__alphabet[letter_index]]
+
     def __pass_through_rotors(self, letter):
-        for rotor in self.__rotors:
-            letter = rotor[letter]
+        for i in range(len(self.__rotors)):
+            letter = self.__pass_through_rotor(i, letter)
 
         letter = self.__reflector[letter]
 
-        for rotor in self.__rotors[::-1]:
-            letter = rotor[letter]
+        for i in range(len(self.__rotors) - 1, -1, -1):
+            letter = self.__pass_through_rotor(i, letter)
 
         return letter
 
