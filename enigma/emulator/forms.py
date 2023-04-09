dfbea518f3a8a6ede58dd49d669980fdafd95c80
boris1592 <buzin.boria@gmail.com>
2023-04-09 15:34:28 +0300

    Added plugs and positions to form

--- enigma/emulator/forms.py
+++ enigma/emulator/forms.py
@@ -10,18 +10,25 @@ class EnigmaConfigForm(forms.Form):
         initial=f'{ascii_uppercase}\n{ascii_uppercase}\n{ascii_uppercase}',
     )
     reflector = forms.CharField(initial=ascii_uppercase)
+    plugs = forms.CharField(initial='AB DC')
+    positions = forms.CharField(initial='1 2 3')
 
     def clean(self):
         cleaned_data = super().clean()
         alphabet = cleaned_data.get('alphabet')
         rotors = cleaned_data.get('rotors')
         reflector = cleaned_data.get('reflector')
+        plugs = cleaned_data.get('plugs')
+        positions = cleaned_data.get('positions')
+
+        if any([not p.isnumeric() for p in positions.split()]):
+            raise forms.ValidationError('Positions should be numbers')
 
         self.config = EnigmaConfig(
             rotors.replace('\r', '').split('\n'),
             reflector,
-            None,
-            None,
+            [tuple(pair) for pair in plugs.split()],
+            [int(p) for p in positions.split()],
             alphabet,
         )
 
